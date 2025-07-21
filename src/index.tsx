2fdf068b1757626e8d2f8f4cd81dae1c7d18d895
dlysenko <dlysenko-contractor@onlocationexp.com>
2025-07-21 16:50:25 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import ReactDOM from "react-dom/client";
 
 import TPSMapApp from "./dev/components/TPSMapApp";
 
-export function renderApp() {
-  const rootElement = document.getElementById("root");
-  if (!rootElement) {
-    throw new Error("Root element not found");
-  }
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(<TPSMapApp />);
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
 }
+const root = ReactDOM.createRoot(rootElement);
+root.render(<TPSMapApp />);
